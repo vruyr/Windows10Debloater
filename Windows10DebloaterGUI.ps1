f632a10de55a4f96c30c3930489ea7da5315abf1
Vruyr Gyolchanyan <vruyr@vruyr.com>
2019-08-26 15:01:01 -0400

    Updated whitelist in Windows10DebloaterGUI.ps1

--- Windows10DebloaterGUI.ps1
+++ Windows10DebloaterGUI.ps1
@@ -90,30 +90,30 @@ $global:Bloatware = @(
 #Valuable Windows 10 AppX apps that most people want to keep. Protected from DeBloat All.
 #Credit to /u/GavinEke for a modified version of my whitelist code
 $global:WhiteListedApps = @(
-    "Microsoft.WindowsCalculator"               # Microsoft removed legacy calculator
+    # "Microsoft.WindowsCalculator"               # Microsoft removed legacy calculator
     "Microsoft.WindowsStore"                    # Issue 1
-    "Microsoft.Windows.Photos"                  # Microsoft disabled/hid legacy photo viewer
+    # "Microsoft.Windows.Photos"                  # Microsoft disabled/hid legacy photo viewer
     "CanonicalGroupLimited.UbuntuonWindows"     # Issue 10
-    "Microsoft.Xbox.TCUI"                       # Issue 25, 91  Many home users want to play games
-    "Microsoft.XboxApp"
-    "Microsoft.XboxGameOverlay"
-    "Microsoft.XboxGamingOverlay"               # Issue 25, 91  Many home users want to play games
-    "Microsoft.XboxIdentityProvider"            # Issue 25, 91  Many home users want to play games
-    "Microsoft.XboxSpeechToTextOverlay"
-    "Microsoft.MicrosoftStickyNotes"            # Issue 33  New functionality.
-    "Microsoft.MSPaint"                         # Issue 32  This is Paint3D, legacy paint still exists in Windows 10
-    "Microsoft.WindowsCamera"                   # Issue 65  New functionality.
+    # "Microsoft.Xbox.TCUI"                       # Issue 25, 91  Many home users want to play games
+    # "Microsoft.XboxApp"
+    # "Microsoft.XboxGameOverlay"
+    # "Microsoft.XboxGamingOverlay"               # Issue 25, 91  Many home users want to play games
+    # "Microsoft.XboxIdentityProvider"            # Issue 25, 91  Many home users want to play games
+    # "Microsoft.XboxSpeechToTextOverlay"
+    # "Microsoft.MicrosoftStickyNotes"            # Issue 33  New functionality.
+    # "Microsoft.MSPaint"                         # Issue 32  This is Paint3D, legacy paint still exists in Windows 10
+    # "Microsoft.WindowsCamera"                   # Issue 65  New functionality.
     "\.NET"
     "Microsoft.HEIFImageExtension"              # Issue 68
-    "Microsoft.ScreenSketch"                    # Issue 55: Looks like Microsoft will be axing snipping tool and using Snip & Sketch going forward
+    # "Microsoft.ScreenSketch"                    # Issue 55: Looks like Microsoft will be axing snipping tool and using Snip & Sketch going forward
     "Microsoft.StorePurchaseApp"                # Issue 68
     "Microsoft.VP9VideoExtensions"              # Issue 68
     "Microsoft.WebMediaExtensions"              # Issue 68
     "Microsoft.WebpImageExtension"              # Issue 68
     "Microsoft.DesktopAppInstaller"             # Issue 68
-    "WindSynthBerry"                            # Issue 68
-    "MIDIBerry"                                 # Issue 68
-    "Slack"                                     # Issue 83
+    # "WindSynthBerry"                            # Issue 68
+    # "MIDIBerry"                                 # Issue 68
+    # "Slack"                                     # Issue 83
 )
 
 #NonRemovable Apps that where getting attempted and the system would reject the uninstall, speeds up debloat and prevents 'initalizing' overlay when removing apps
