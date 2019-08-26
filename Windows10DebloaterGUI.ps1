8e724d2cb7b5e5c589c866f545ac27f6dad0f384
Vruyr Gyolchanyan <vruyr@vruyr.com>
2019-08-26 15:09:36 -0400

    Added elevation to Windows10DebloaterGUI.ps1 script.

--- Windows10DebloaterGUI.ps1
+++ Windows10DebloaterGUI.ps1
@@ -1,4 +1,17 @@
 #This will self elevate the script so with a UAC prompt since this script needs to be run as an Administrator in order to function properly.
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+    Write-Host "You didn't run this script as an Administrator. This script will self elevate to run as an Administrator and continue."
+    Start-Sleep 1
+    Write-Host "                                               3"
+    Start-Sleep 1
+    Write-Host "                                               2"
+    Start-Sleep 1
+    Write-Host "                                               1"
+    Start-Sleep 1
+    Start-Process powershell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
+    Exit
+}
+
 
 $ErrorActionPreference = 'silentlycontinue'
 
